d097e8b06613cbaa140f4d84915f2f6c934ec48d
ServeIron <servetgkdmr@gmail.com>
2022-02-17 17:41:46 +0300

    fetch okey

--- js/fetch/app.js
+++ js/fetch/app.js
@@ -1,2 +1,57 @@
 const book = new BOOK();
-console.log("profile ",book.getOrder());
+const ui = new UI();
+
+setInterval(function () {getBook()}, 1000);
+function getBook(){
+ book.getOrder().then( response=> { 
+     ui.createBook(response.deepthResponsive);
+     ui.createTrades(response.bookResponsive);
+    }).catch(error => {console.log("hata",error)})
+}
+getBook()
+var buy_buton = document.getElementById("buy_buton");
+var normal_buton = document.getElementById("normal_buton");
+var sell_buton = document.getElementById("sell_buton");
+
+var order_sell = document.getElementById("order_sell");
+var order_buy = document.getElementById("order_buy");
+
+var visible_order = "visible-order";
+var un_visible_order = "unvisible-order";
+var visib_order = "visib-order";
+
+buy_buton.addEventListener('click', e => {
+
+    classChange(order_buy,visib_order);
+    classChange(order_buy,un_visible_order);
+    classChange(order_buy,visible_order,"add");
+
+    classChange(order_sell,un_visible_order,"add");
+});
+
+sell_buton.addEventListener('click', e => {
+  
+    classChange(order_sell,visib_order);
+    classChange(order_sell,un_visible_order);
+    classChange(order_sell,visible_order,"add");
+
+    classChange(order_buy,un_visible_order,"add");
+ });
+
+ normal_buton.addEventListener('click', e => {
+  
+    classChange(order_sell,visible_order);   
+    classChange(order_buy,un_visible_order);
+
+    classChange(order_sell,visib_order,"add");
+    classChange(order_buy,visib_order,"add");
+ });
+
+
+ function classChange(doc,name,type = "remove"){
+    if(type == "remove"){
+        doc.classList.remove(name);
+    } else {
+        doc.classList.add(name);
+    }
+}
--- js/fetch/ui.js
+++ js/fetch/ui.js
@@ -1,5 +1,90 @@
 class UI{
     constructor(){
-        
+        this.order_book_bids = document.getElementById("order_book_bids");
+        this.order_book_asks = document.getElementById("order_book_asks");
+        this.order_sell = document.getElementById("order_sell");
+        this.order_buy = document.getElementById("order_buy");
     }
+
+     createBook(object) {
+      
+       this.order_book_bids.innerHTML = " ";
+       this.order_book_asks.innerHTML = " ";
+     
+       object.bids.forEach((element,index) => {
+          const contentDiv = document.createElement('div');    
+
+          const inDiv1 = document.createElement('div');
+          const inDiv2 = document.createElement('div');  
+
+          inDiv1.innerHTML = element[1].substring(0,7);
+          inDiv2.innerHTML = element[0].substring(0,8);
+          contentDiv.appendChild(inDiv1);
+          contentDiv.appendChild(inDiv2);
+  
+          this.order_book_bids.appendChild(contentDiv);  
+         
+        });
+
+        object.asks.forEach((element,index) => {
+            const contentDiv = document.createElement('div');    
+  
+            const inDiv1 = document.createElement('div');
+            const inDiv2 = document.createElement('div');  
+  
+            inDiv1.innerHTML = element[0].substring(0,7);
+            inDiv2.innerHTML = element[1].substring(0,8);
+            contentDiv.appendChild(inDiv1);
+            contentDiv.appendChild(inDiv2);
+    
+            this.order_book_asks.appendChild(contentDiv);  
+           
+          });
+    }
+    createTrades(object){
+      this.order_sell.innerHTML = " ";
+      this.order_buy.innerHTML = " ";
+      object.forEach((element,index) => {
+       if(element.isBuyerMaker == true && element.isBestMatch == true){
+        const contentDiv = document.createElement('div');    
+
+        const inDiv1 = document.createElement('div');
+        const inDiv2 = document.createElement('div');  
+        const inDiv3 = document.createElement('div');  
+
+        contentDiv.classList.add("order-book-list-in");
+
+        inDiv1.innerHTML = element.price.substring(0,7);
+        inDiv2.innerHTML = element.qty.substring(0,8);
+        inDiv3.innerHTML = element.quoteQty.substring(0,8);
+        contentDiv.appendChild(inDiv1);
+        contentDiv.appendChild(inDiv2);
+        contentDiv.appendChild(inDiv3);
+
+        this.order_sell.appendChild(contentDiv);  
+      }
+      if(element.isBuyerMaker == false && element.isBestMatch == true){
+        const contentDiv = document.createElement('div');    
+
+        const inDiv1 = document.createElement('div');
+        const inDiv2 = document.createElement('div');  
+        const inDiv3 = document.createElement('div');  
+
+        contentDiv.classList.add("order-book-list-in");
+    
+
+        inDiv1.innerHTML = element.price.substring(0,7);
+        inDiv2.innerHTML = element.qty.substring(0,8);
+        inDiv3.innerHTML = element.quoteQty.substring(0,8);
+        contentDiv.appendChild(inDiv1);
+        contentDiv.appendChild(inDiv2);
+        contentDiv.appendChild(inDiv3);
+
+        this.order_buy.appendChild(contentDiv);  
+      }
+      
+       
+      });
+    }
+    
 }
